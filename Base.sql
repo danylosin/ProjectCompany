0c086feb52cd4348e6dd1ca573effbc8b4f46524
Dan Sinchenko <danylosi@tahometer.com>
2018-11-13 11:37:45 +0200

    added new select scripts

--- Base.sql
+++ Base.sql
@@ -6,11 +6,17 @@ USE ProjectCompany;
 
 CREATE TABLE projects (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(32) NOT NULL
+    title VARCHAR(32) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL
 );
 
-INSERT INTO projects(title) VALUES
-        ("Project #1"), ("Project #2"), ("Project #3"), ("Project #4"), ("Project #5");
+INSERT INTO projects(title, from_date, to_date) VALUES
+        ("Project #1", "2017-01-01", "2018-01-01"), 
+        ("Project #2", "2017-01-01", "2017-12-01"), 
+        ("Project #3", "2017-01-01", "2018-01-01"), 
+        ("Project #4", "2017-01-01", "2018-01-01"), 
+        ("Project #5", "2017-01-01", "2018-01-01");
 
 CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -26,25 +32,30 @@ CREATE TABLE skills (
 
 INSERT INTO skills(title) VALUES
         ("Angular"), ("React"), ("C#"), ("PhP"), ("Java"), 
-        ("Ruby"), ("Python"), ("Project Management");
+        ("Ruby"), ("Python"), ("Project Management"), ("Linux"), ("Mysql");
 
 CREATE TABLE contributions (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(32) NOT NULL,
     employee_id INT,
     project_id INT,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
     FOREIGN KEY (employee_id)
         REFERENCES employees(id),
     FOREIGN KEY (project_id)
         REFERENCES projects(id)        
 );
 
-INSERT INTO contributions(title, employee_id, project_id) VALUES
-        ("Writing backend on c#", 1, 1), 
-        ("Writing frontend on Angular", 2, 1), 
-        ("Writing backend on Java", 1, 2), 
-        ("Writing frontend", 2, 2), 
-        ("Writing userstories", 3, 2);
+INSERT INTO contributions(title, employee_id, project_id, from_date, to_date) VALUES
+        ("Writing backend on c#", 1, 1, "2017-06-01", "2018-01-01"), 
+        ("Writing frontend on Angular", 2, 1, "2017-02-01", "2017-04-01"), 
+        ("Writing backend on Java", 1, 2, "2017-05-01", "2017-08-01"), 
+        ("Writing frontend", 2, 2, "2017-04-01", "2017-07-01"), 
+        ("Writing userstories", 3, 2, "2017-02-01", "2017-03-01"),
+        ("Configurating server", 1, 1, "2017-08-01", "2017-08-03"),
+        ("Creating database", 1, 1, "2017-08-10", "2017-08-15")
+        ;
 
 CREATE TABLE employees_skills (
     employee_id INT NOT NULL,
@@ -55,7 +66,7 @@ CREATE TABLE employees_skills (
         REFERENCES skills(id)
 );
 
-INSERT INTO employees_skills VALUES (1, 3), (2, 1), (3, 8), (1, 5);
+INSERT INTO employees_skills VALUES (1, 3), (2, 1), (3, 8), (1, 5), (1, 9), (1, 10);
 
 CREATE TABLE contributions_skills (
     contribution_id INT NOT NULL,
@@ -66,4 +77,4 @@ CREATE TABLE contributions_skills (
         REFERENCES skills(id)
 );
 
-INSERT INTO contributions_skills VALUES(1, 3), (2, 1), (3, 5);
+INSERT INTO contributions_skills VALUES(1, 3), (2, 1), (3, 5), (4, 2), (5, 8), (6, 9), (7, 10);
--- Select.sql
+++ Select.sql
@@ -3,7 +3,7 @@ SELECT "Info about employee";
 SELECT employees.name, 
         contributions.title as `Contribution title`, 
         projects.title as `Project title`,
-        skills.title as `Skill title` 
+        skills.title as `Contribution skill` 
 FROM employees 
 JOIN contributions ON employees.id = contributions.employee_id 
 JOIN projects ON contributions.project_id = projects.id
@@ -17,7 +17,6 @@ SELECT employees.name
 FROM employees 
 LEFT JOIN contributions ON  employees.id = contributions.employee_id WHERE contributions.employee_id is null;
 
-
 SELECT "Info about project";
 
 SELECT projects.title as `Project title`,
@@ -30,3 +29,79 @@ JOIN contributions_skills ON contributions_skills.contribution_id = contribution
 JOIN skills ON contributions_skills.skill_id = skills.id
 JOIN employees ON contributions.employee_id = employees.id
 WHERE projects.id = 1;
+
+/*
+
+
+
+SELECT projects.title as `Project title`,
+        projects.from_date,
+        projects.to_date,
+        COUNT(contributions.id) as count_of_contributions
+FROM projects
+JOIN contributions ON 
+        contributions.project_id = projects.id AND 
+        (contributions.from_date between '2017-01-01' AND '2017-05-11') 
+GROUP BY (projects.id)
+ORDER BY count_of_contributions DESC
+LIMIT 3;  
+*/
+
+SET @value = '2017';
+SELECT CONCAT("Top 3 projects by count of contribution by ", @value) AS concated_string;
+
+SELECT projects.id as 'Project id',
+        projects.title as `Project title`,
+        projects.from_date,
+        projects.to_date,
+        COUNT(contributions.id) as count_of_contributions
+FROM projects
+JOIN contributions ON 
+        contributions.project_id = projects.id      
+        AND
+        @value = YEAR(contributions.to_date)
+GROUP BY (projects.id)
+ORDER BY count_of_contributions DESC
+LIMIT 3;    
+
+SET @value = '2017';
+SELECT CONCAT("Top 3 projects by count of employee by ", @value) AS concated_string;
+
+SELECT projects.id as 'Project id',
+        projects.title as `Project title`,
+        projects.from_date,
+        projects.to_date,
+        COUNT(DISTINCT employees.id) as count_of_employees
+FROM projects
+JOIN contributions ON 
+        contributions.project_id = projects.id      
+        AND
+        @value = YEAR(contributions.to_date)
+JOIN employees ON 
+        employees.id = contributions.employee_id        
+GROUP BY (projects.id)
+ORDER BY count_of_employees DESC
+LIMIT 3; 
+
+
+SET @from_date = '2017-03-01';
+SET @to_date = '2017-10-01';
+
+SET @string = CONCAT("Get activity of employee from ", @from_date, " to ", @to_date);
+SELECT @string as 'Activity of employee';
+
+SELECT employees.name, 
+        contributions.title as `Contribution title`, 
+        projects.title as `Project title`,
+        skills.title as `Contribution skill`,
+        contributions.from_date,
+        contributions.to_date  
+FROM employees 
+JOIN contributions ON 
+        employees.id = contributions.employee_id 
+        AND 
+        (contributions.from_date >= @from_date AND contributions.to_date <= @to_date)
+JOIN projects ON contributions.project_id = projects.id
+JOIN contributions_skills ON contributions.id = contributions_skills.contribution_id
+JOIN skills ON contributions_skills.skill_id = skills.id  
+WHERE employees.id = 1;
