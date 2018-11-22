37ef78cbe3eb4cd1d0097b13602ec2a870433829
Dan Sinchenko <danylosi@tahometer.com>
2018-11-22 17:56:59 +0200

    create conribution-list component

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { routes } from './app.routes';
 import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProjectDetailComponent } from './project/project-detail/project-detail.component';
+import { ContributionListComponent } from './contribution/contribution-list/contribution-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectListComponent,
     NavBarComponent,
-    ProjectDetailComponent
+    ProjectDetailComponent,
+    ContributionListComponent
   ],
   imports: [
     BrowserModule,
